3accee7875d683229e5fc792c12d37e1464fbf70
Ulrich Diedrichsen <uli@moinsen.dev>
2025-12-17 12:09:49 +0100

    devhub 0.3.0

--- Formula/devhub.rb
+++ Formula/devhub.rb
@@ -1,16 +1,16 @@
 class Devhub < Formula
   desc "Multi-project development environment manager"
   homepage "https://github.com/moinsen-dev/devhub"
-  version "0.2.0"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/moinsen-dev/devhub/releases/download/v0.2.0/devhub-aarch64-apple-darwin.tar.gz"
-      sha256 "e31190f47dc35fea3e5c80d76eb733d673b306f2c900d0a6596f8b5d9df1ffc1"
+      url "https://github.com/moinsen-dev/devhub/releases/download/v0.3.0/devhub-aarch64-apple-darwin.tar.gz"
+      sha256 "f5618ab1235468bb90751feb23cbbf837e6747306ed6490deed68b36721f87d2"
     else
-      url "https://github.com/moinsen-dev/devhub/releases/download/v0.2.0/devhub-x86_64-apple-darwin.tar.gz"
-      sha256 "c2b0c4ff58d6e605da24c9245e8b347f1f02f20325b8aab4e7d1fc061bca9295"
+      url "https://github.com/moinsen-dev/devhub/releases/download/v0.3.0/devhub-x86_64-apple-darwin.tar.gz"
+      sha256 "07b2fad0436de5f80dbfddc18e14863ec758e9fe80565a427d8ecc3d6ddf96be"
     end
   end
 
